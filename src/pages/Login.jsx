12d8233128461dcecdda71b4ad458160ad5119f5
Bryan Chau <bryanchau@Bryans-MacBook-Air-2.local>
2023-04-10 00:07:59 -0700

    added theme to login/signup & implemented mixin for scss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -66,30 +66,30 @@ const Login = (props) => {
  };
  
  return (
- <form style={{ display: "flex", flexDirection: "column", maxWidth: "300px", margin: "auto" }}>
-   <h1>Login</h1>
-   <TextField
-     label="Email"
-     type="email"
-     variant="outlined"
-     name="email"
-     value={form.email}
-     onChange={onFormInputChange}
-     style={{ marginBottom: "1rem" }}
-   />
-   <TextField
-     label="Password"
-     type="password"
-     variant="outlined"
-     name="password"
-     value={form.password}
-     onChange={onFormInputChange}
-     style={{ marginBottom: "1rem" }}
-   />
-   <Button variant="contained" color="primary" onClick={onSubmit}>
-     Login
-   </Button>
-   <p>Don't have an account? <Link to="/signup">Signup</Link></p>
+ <form className="loginForm">
+    <h1>Login</h1>
+    <TextField
+      label="Email"
+      type="email"
+      variant="outlined"
+      name="email"
+      value={form.email}
+      onChange={onFormInputChange}
+      style={{ marginBottom: "1rem" }}
+    />
+    <TextField
+      label="Password"
+      type="password"
+      variant="outlined"
+      name="password"
+      value={form.password}
+      onChange={onFormInputChange}
+      style={{ marginBottom: "1rem" }}
+    />
+    <Button variant="contained" color="primary" onClick={onSubmit}>
+      Login
+    </Button>
+    <p>Don't have an account? <Link to="/signup">Signup</Link></p>
  </form>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -37,31 +37,33 @@ const Signup = () => {
    }
  };
  
- return <form style={{ display: "flex", flexDirection: "column", maxWidth: "300px", margin: "auto" }}>
-   <h1>Signup</h1>
-   <TextField
-     label="Email"
-     type="email"
-     variant="outlined"
-     name="email"
-     value={form.email}
-     onInput={onFormInputChange}
-     style={{ marginBottom: "1rem" }}
-   />
-   <TextField
-     label="Password"
-     type="password"
-     variant="outlined"
-     name="password"
-     value={form.password}
-     onInput={onFormInputChange}
-     style={{ marginBottom: "1rem" }}
-   />
-   <Button variant="contained" color="primary" onClick={onSubmit}>
-     Signup
-   </Button>
-   <p>Have an account already? <Link to="/login">Login</Link></p>
+ return(
+  <form className="loginForm">
+      <h1>Signup</h1>
+      <TextField
+        label="Email"
+        type="email"
+        variant="outlined"
+        name="email"
+        value={form.email}
+        onInput={onFormInputChange}
+        style={{ marginBottom: "1rem" }}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        variant="outlined"
+        name="password"
+        value={form.password}
+        onInput={onFormInputChange}
+        style={{ marginBottom: "1rem" }}
+      />
+      <Button variant="contained" color="primary" onClick={onSubmit}>
+        Signup
+      </Button>
+      <p>Have an account already? <Link to="/login">Login</Link></p>
  </form>
+ )
 }
  
 export default Signup;
